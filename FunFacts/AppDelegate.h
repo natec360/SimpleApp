bf181afbe3328b37b82a0dad2cac75e7296d31fa
natec360 <natec360@gmail.com>
2015-08-26 14:12:02 -0400

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Nathan Chiu on 8/26/15.
+//  Copyright (c) 2015 Nathan Chiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FunFacts
+//
+//  Created by Nathan Chiu on 8/26/15.
+//  Copyright (c) 2015 Nathan Chiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
